31e25cb5ef216deafa6a20288ebba996a5761890
Naveen3884 <naveengorantla3@gmail.com>
2019-07-22 23:14:10 +0530

    For WebService exposing

---
+++ src/main/java/com/rbc/webservices/RunServiceApp.java
@@ -0,0 +1,11 @@
+package com.rbc.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunServiceApp {
+    public static void main(String... args){
+        SpringApplication.run(RunServiceApp.class);
+    }
+}
